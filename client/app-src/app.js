737ef360c72d80136339b984798649820f15e23a
rafaelwaterkemper <rafaelwaterkemper@gmail.com>
2019-10-05 22:06:32 -0300

    Add loaders

--- client/app-src/app.js
+++ client/app-src/app.js
@@ -1,5 +1,7 @@
 import { NegociacaoController } from './controllers/NegociacaoController.js';
 import { Negociacao } from './domain/index.js';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 
 const controller = new NegociacaoController();
 const negociacao = new Negociacao(new Date(), 1, 200);
@@ -14,5 +16,5 @@ const config = {
     body 
 };
 
-fetch('/negociacoes', config)
+fetch('http://localhost:3000/negociacoes', config)
     .then(() => console.log('Dado enviado com sucesso'));
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,7 +4,8 @@ module.exports = {
     entry: './app-src/app.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: 'dist'
     },
     module: {
         rules: [
@@ -13,6 +14,37 @@ module.exports = {
                 exclude: /node_modules/,
                 use: {
                     loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader' }
+                ]
+            },
+            {
+                test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+                use: {
+                    loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+                }
+            },
+            {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                use: {
+                    loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
+                }
+            },
+            {
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                use: {
+                    loader: 'file-loader'
+                }
+            },
+            {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                use: {
+                    loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
                 }
             }
         ]
